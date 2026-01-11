f5dcda41d487620a49fb6efaef6852f1fc65e10a
Edwin Kofler <edwin@kofler.dev>
2026-01-11 00:09:47 -0800

    Remove CJS bundle and publish

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+export default config
+declare namespace config {
+	let plugins: string[]
+	let printWidth: number
+	let useTabs: boolean
+	let semi: boolean
+	let singleQuote: boolean
+	let jsxSingleQuote: boolean
+	let trailingComma: string
+	let arrowParens: string
+	let endOfLine: string
+}
